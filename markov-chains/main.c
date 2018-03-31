c1b5996346a3298588ce3fd4633da73f2786f8f1
Henrique Borges <henriqueborges@henriques-air>
2018-03-31 17:54:51 -0300

    Initial Commit

---
+++ markov-chains/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  markov-chains
+//
+//  Created by Henrique Borges on 31/03/18.
+//  Copyright © 2018 Henrique Borges. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
