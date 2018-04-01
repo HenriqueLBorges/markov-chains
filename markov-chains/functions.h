4c204846208b01f7e26e59b55a00b09fe9206aaa
Henrique Borges <henriqueborges@henriques-air.home>
2018-04-01 15:26:56 -0300

    first commit

---
+++ markov-chains/functions.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  markov-chains
+//
+//  Created by Henrique Borges on 31/03/18.
+//  Copyright © 2018 Henrique Borges. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+void testMarkovChain();
+
+#endif /* Header_h */
